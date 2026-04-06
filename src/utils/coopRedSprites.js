c3fa587d5d935dd995ffc6114530505b358a4745
anthonypruneta <antho.pruneta@gmail.com>
2026-04-06 22:12:04 +0200

    Update coopRedSprites.js

--- src/utils/coopRedSprites.js
+++ src/utils/coopRedSprites.js
@@ -5,13 +5,13 @@
 import urlBulbizarre from '../assets/coop/bulbizarre.png';
 import urlCarapuce from '../assets/coop/carapuce.png';
 import urlDracaufeu from '../assets/coop/dracaufeu.png';
-import urlFlorizarre from '../assets/coop/Florizarre.png';
+import urlFlorizarre from '../assets/coop/florizarre.png';
 import urlLokhlass from '../assets/coop/lokhlass.png';
 import urlPikachu from '../assets/coop/pikachu.png';
 import urlRed from '../assets/coop/red.png';
 import urlRonflex from '../assets/coop/ronflex.png';
 import urlSalameche from '../assets/coop/salameche.png';
-import urlTortank from '../assets/coop/Tortank.png';
+import urlTortank from '../assets/coop/tortank.png';
 
 const urlByFileLower = {
   'bulbizarre.png': urlBulbizarre,
